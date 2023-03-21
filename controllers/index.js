66d93b6847840f6b5e95c67128a672c00738bc3a
wiranley <wiranley@outlook.com>
2023-03-21 04:32:34 -0300

    Initial Commit

---
+++ controllers/index.js
@@ -0,0 +1,18 @@
+let fields = document.querySelectorAll('#form-user-create [name]');
+let user = {};
+
+fields.forEach((field, index) => {
+
+    if(field.name === 'gender'){
+
+        if(field.checked){
+            user[field.name] = field.value
+        }
+       
+    }else{
+        user[field.name] = field.value
+    }
+
+});
+
+console.log(user)
