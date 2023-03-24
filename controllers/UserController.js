1c9b3415f6ce5980b139b09b01ff1bd672b2b495
wiranley <wiranley@outlook.com>
2023-03-24 06:36:16 -0300

    Switching between create and update form

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,17 @@ class UserController {
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
-        this.onSubmit()
+        this.onSubmit();
+        this.onEdit();
+
+    }
+
+    onEdit(){
+        document.querySelector('#box-user-update .btn-cancel').addEventListener('click', e=>{
+
+            this.showPanelCreate()
+
+        });
 
     }
 
@@ -146,17 +156,38 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
+
+        tr.querySelector('.btn-edit').addEventListener('click', e=>{
+            console.log(JSON.parse(tr.dataset.user));
+
+            this.showPanelUpdate();
+
+        })
 
         this.tableEl.appendChild(tr);
 
         this.updateCount();
 
 
+    }
+
+    showPanelCreate(){
+
+        document.querySelector('#box-user-create').style.display = 'block';
+        document.querySelector('#box-user-update').style.display = 'none';
+
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector('#box-user-create').style.display = 'none';
+        document.querySelector('#box-user-update').style.display = 'block';
+        
     }
 
     updateCount(){
